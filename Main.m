c69b6f25909d80f037357f56615029cf52ffec2b
Kimberly Winter <kwinter@olin.edu>
2019-03-14 22:05:51 -0400

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,7 +1,7 @@
 %%Kimberly Winter                       3/12/19
 %Main function OFDM
 
-%TODO: Generate message to send through channel using generateMessage
+%Generate message to send through channel using generateMessage
 bufferSize=50;
 
 header=generateRand(64*100);
@@ -15,15 +15,26 @@ receivedMess=nonflat_channel(mess2send);
 
 %Estimate H
 %Trim header and divide by known header
-[correlation,lag] = xcorr(receivedMess,header);
+[correlation,lag] = xcorr(receivedMess,mess2send(1:160));
 [M,I]=max(abs(correlation));
 lagDiff=lag(I);
-trimmedMessage=receivedMess(-lagDiff+1:(-1*lagDiff)+length(mess2send));
+trimmedMessage=receivedMess(lagDiff+1:(lagDiff)+length(mess2send)-(2*bufferSize));
 
-%TODO: Trim signal and take last 64 samples
-
-%TODO: take FFT of trimmed signal
-
-%TODO: divide by
+%take FFT of trimmed signal for header
+for i=1:(length(mess2send)-(2*bufferSize))/80
+    final(64*(i-1)+1:64*(i-1)+64)= ifft(trimmedMessage((i-1)*64+17:(i-1)*64+80));
+end
 
 %Divide Yk/Hk
+HEstimate=mean(abs(final(1:6400).'./header));
+
+MessageEstimate=final(6401:6464)/HEstimate;
+normalEst=normalize(MessageEstimate);
+
+error=0;
+for i=1:length(normalEst)
+    if(normalEst(i)~=message(i))
+        error=error+1
+        i
+    end
+end
